7fb3d90651777a715e402e62a78658c0c03c048b
Dimas727hehe <dmskrmain1029@gmail.com>
2025-10-03 05:26:04 +0700

    Tugas 1

--- jobsheet5/ifCetakKrs10.java
+++ jobsheet5/ifCetakKrs10.java
@@ -8,14 +8,18 @@ public class ifCetakKrs10 {
         System.out.println("---Cetak KRS SIAKAD---");
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
         boolean uktLunas =sc.nextBoolean();
+        String t;
 
-        if(uktLunas) {
+        /* if(uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi.");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA.");
         } else {
             System.out.println("Registrasi ditolak. Silahkan selesaikan pembayaran UKT terlebih dahulu.");
-        }
+        } */
 
+        t = (uktLunas) ? "Pembayaran UKT terverifikasi.\nSilahkan cetak KRS dan minta tanda tangan DPA." : "Registrasi ditolak. Silahkan selesaikan pembayaran UKT terlebih dahulu.";
+
+        System.out.println(t);
         sc.close();
     }
 }
