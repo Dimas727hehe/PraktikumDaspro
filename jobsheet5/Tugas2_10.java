6e457c9226930ad8099b51bc00b5ac8befbbba90
Dimas727hehe <dmskrmain1029@gmail.com>
2025-10-03 05:38:03 +0700

    Tugas 2

---
+++ jobsheet5/Tugas2_10.java
@@ -0,0 +1,17 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class Tugas2_10 {
+    public static void main(String[] args) {
+        Scanner S = new Scanner(System.in);
+        System.out.print("Masukkan angka:");
+        int angka = S.nextInt();
+
+        if(angka % 2 == 0) {
+            System.out.println("Angka yang anda inputkan (" + angka + ") merupakan bilangan genap.");
+        } else {
+            System.out.println("Angka yang anda inputkan (" + angka + ") merupakan bilangan ganjil.");
+        }
+    }
+}
