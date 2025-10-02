75946bdb481704836783b7c67377095fe98d2ecb
Dimas727hehe <dmskrmain1029@gmail.com>
2025-10-03 04:06:26 +0700

    Modifikasi Percobaan 1

--- jobsheet5/ifCetakKrs10.java
+++ jobsheet5/ifCetakKrs10.java
@@ -12,6 +12,8 @@ public class ifCetakKrs10 {
         if(uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi.");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA.");
+        } else {
+            System.out.println("Registrasi ditolak. Silahkan selesaikan pembayaran UKT terlebih dahulu.");
         }
 
         sc.close();
